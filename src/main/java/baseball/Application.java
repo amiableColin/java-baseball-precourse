61dd38849b6c2055bcb564dbc871211796bd1593
amiableColin <142316711+amiableColin@users.noreply.github.com>
2024-07-15 18:49:07 +0900

    feat: 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,58 @@
 package baseball;
+import java.util.Scanner;
+import java.util.Random;
 
 public class Application {
     public static void main(String[] args) {
-        //TODO: 숫자 야구 게임 구현
+        Scanner s = new Scanner(System.in);
+        Random r = new Random();
+        char[] user_input;
+        int restart = 0;
+        while (restart != 2) {
+            String correctAnswer = Integer.toString(r.nextInt(10));
+            String temp = "";
+            while (true) {
+                temp = Integer.toString(r.nextInt(10));
+                if (!correctAnswer.contains(temp)) correctAnswer += temp;
+                if (correctAnswer.length() == 3) break;
+            }
+            System.out.println("생성된 숫자: " + correctAnswer); // 디버깅용
+
+            while (true) {
+                System.out.print("숫자를 입력해주세요 : ");
+                user_input = s.next().toCharArray();
+                int strike = 0;
+                int ball = 0;
+                // 스트라이크 찾는 로직
+                for (int i = 0; i < user_input.length; i++) {
+                    if (correctAnswer.indexOf(user_input[i]) == i) {
+                        strike++;
+                    }
+                    else if (correctAnswer.indexOf(user_input[i]) != -1) {
+                        ball++;
+                    }
+                }
+                if (ball != 0 || strike != 0) {
+                    if (ball != 0) {
+                        System.out.print(ball + "볼 ");
+                    }
+                    if (strike != 0) {
+                        System.out.print(strike + "스트라이크");
+                    }
+                }
+                else {
+                    System.out.print("낫싱");
+                }
+                System.out.println();
+
+                if (strike == 3) {
+                    System.out.println("3개의 숫자를 모두 맞추셨습니다! 게임 종료");
+                    System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+                    restart = s.nextInt();
+                    break;
+                }
+            }
+        }
+        s.close();
     }
 }
